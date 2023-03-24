d821140d3e977279f32f20afe529d04ec55b5177
Ben Coomes <bencoomes@github.com>
2023-03-24 10:34:52 -0400

    pointers pass by reference

--- 001-memory-and-data-semantics/pointers/pointers.go
+++ 001-memory-and-data-semantics/pointers/pointers.go
@@ -5,14 +5,25 @@ import "fmt"
 func main() {
 	fmt.Println("POINTERS ON POINTERS")
 
-	// pass by value
 	count := 10
+
+	println("pass by value")
 	println("count - Value: ", count, "Addr: ", &count)
 	increment_by_value(count)
+	println("count - Value: ", count, "Addr: ", &count)
+
+	println("pass by reference")
+	println("count - Value: ", count, "Addr: ", &count)
+	increment_by_reference(&count)
 	println("count - Value: ", count, "Addr: ", &count)
 }
 
 func increment_by_value(i int) {
 	i += 1
 	println("i     - Value: ", i, "Addr: ", &i)
+}
+
+func increment_by_reference(i *int) {
+	*i += 1
+	println("i     - Value: ", *i, "Addr: ", i)
 }
